4b35f4752f1acf0855ba53d6b6aa54afcaf8af67
cyace84 <cyace84@gmail.com>
2022-04-15 16:56:08 +0300

    added run script

---
+++ run.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+from random import randint
+from time import sleep
+from configparser import ConfigParser
+
+BOT_CFG = 'bot.cfg'
+parser = ConfigParser()
+parser.read(BOT_CFG)
+cfg = parser["config"]
+
+spam_interval = randint(1, 8)
+
+app = Client('session', api_id=cfg["api_id"], api_hash=cfg["api_hash"])
+
+with app:
+    for i in range(int(cfg["spam_count"])):
+        app.send_message(cfg["chat_id"], cfg["text"])
+        sleep(spam_interval)
+
+if __name__ == "__main__":
+    app.run()
